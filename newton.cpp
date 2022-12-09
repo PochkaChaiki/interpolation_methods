dc3362604bf9f0cbfa616ad640cf3f362efc4fce
kinokiken <figoroki@mail.ru>
2022-12-09 18:15:11 +0300

    Newton's method v1
    Добавил интерполяционный метод Ньютона

---
+++ newton.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
+struct Point
+{
+    double x; 
+    double y;
+};
+
+vector <double> Newton(vector <Point> p, vector <double> newx)
+{
+    vector <double> result;
+    result.reserve(newx.size());
+    for (int l = 0; l < newx.size(); l++)
+    {
+		double sum = p[0].y;
+        for(int i = 1; i < p.size(); ++i)
+        {
+            double F = 0;
+            for(int j = 0; j <= i; ++j)
+            {    
+                double den = 1;
+                for(int k = 0; k <= i; ++k)
+                    if (k != j) 
+                        den *= (p[j].x - p[k].x);
+                F += p[j].y/den;
+            }
+            for(int k = 0; k < i; ++k)
+                F *= (newx[l] - p[k].x);
+            sum += F;
+        }
+        result.push_back(sum);
+    }
+    return result;
+}
