6848f401bd6d32aadebef0dfbee81b32ea65d361
S1ngSal <azza20031and1@gmail.com>
2022-12-13 00:40:03 +0300

    Багфикс

--- linear.cpp
+++ linear.cpp
@@ -13,10 +13,10 @@ vector<double> Linear(vector <Point> train, vector<double> test)
 {
 	vector<double> ytest;
 	ytest.reserve(test.size());
-	for (int i = 0; i < train.size(); i++) {
-		for (int j = 0; j < test.size(); j++) {
-			if (test[j] > train[i].x) {
-				ytest.push_back(train[i].y + ((train[i + 1].y - train[i].y)/(train[i+1].x-train[i].x))/(test[j]-train[i].x));
+	for (int i = 0; i < test.size(); ++i) {
+		for (int j = 0; j < train.size(); ++j) {
+			if (test[i] > train[j].x) {
+				ytest.push_back(train[j].y + (test[i]-train[j].x)*(train[j+1].y-train[j].y)/(train[j+1].x-train[j].x));
 				break;
 			}
 		}
